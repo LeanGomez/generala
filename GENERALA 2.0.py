e2f50846c2789dbf6dbbafb9b8aa700e4355fe26
Rakatek <leandrodanielg97@gmail.com>
2019-03-07 18:29:01 -0300

    Para mas jugadores

--- GENERALA 2.0.py
+++ GENERALA 2.0.py
@@ -17,6 +17,9 @@ def es_full(tirada):  # dice si una tirada ORDENADA es un full
 def es_escalera(tirada):  # dice si una tirada ORDENADA es una escalera
     return tirada == [1, 2, 3, 4, 5] or tirada == [2, 3, 4, 5, 6] or tirada == [3, 4, 5, 6, 1]
 
+def sin_juego(tirada): #dice si una tirada ORDENADA no tiene juego
+    return tirada != es_escalera(tirada) and tirada != es_full(tirada) and tirada != es_generala(tirada) \
+           and tirada != es_poker(tirada)
 
 def tirar(tirada):
     tirada = sorted(tirada)
@@ -32,10 +35,18 @@ def elegir_reroll(tirada):
         tirada[dado - 1] = rnd.randint(1, 6)
     return tirada
 
-
+puntos = []
 tirada = []
 for i in range(1, 6):
     tirada.append(rnd.randint(1, 6))
+
+jugador_1 = input('Escribir el nombre del jugador 1\n')
+
+jugador_2 = input('Escribir el nombre del jugador 2\n')
+
+jugador_3 = input('Escribir el nombre del jugador 3\n')
+
+jugador_4 = input('Escribir el nombre del jugador 4\n')
 
 tirada = tirar(tirada)
 
@@ -44,13 +55,20 @@ for i in range(0,2):
     tirada = tirar(tirada)
 
 if es_generala(tirada):
+    puntos + 50
     print("salio generala")
 
 if es_full(tirada):
+    puntos + 20
     print("salio full")
 
 if es_poker(tirada):
+    puntos + 35
     print("salio poker")
 
 if es_escalera(tirada):
+    puntos + 25
     print("salio escalera")
+
+if sin_juego(tirada):
+    print('YOU LOOSE')
